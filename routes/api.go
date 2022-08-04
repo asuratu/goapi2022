a71d5b8dc15e4ad00ba8caacd5d15dfd414ec664
AsuraTu <455224159@qq.com>
2022-08-04 15:49:11 +0800

    手机是否注册接口

--- routes/api.go
+++ routes/api.go
@@ -2,23 +2,29 @@
 package routes
 
 import (
-    "net/http"
+	"gohub/app/http/controllers/api/v1/auth"
+	"net/http"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 // RegisterAPIRoutes 注册网页相关路由
 func RegisterAPIRoutes(r *gin.Engine) {
 
-    // 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
-    v1 := r.Group("/v1")
-    {
-        // 注册一个路由
-        v1.GET("/", func(c *gin.Context) {
-            // 以 JSON 格式响应
-            c.JSON(http.StatusOK, gin.H{
-                "Hello": "World!",
-            })
-        })
-    }
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello world",
+		})
+	})
+
+	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
+	v1 := r.Group("/v1")
+	{
+		authGroup := v1.Group("/auth")
+		{
+			suc := new(auth.SignupController)
+			// 判断手机是否已注册
+			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+		}
+	}
 }
