c66e9eb4e5cb3e8147fa913418553e9781ea5fcf
AsuraTu <455224159@qq.com>
2022-08-05 22:06:15 +0800

    密码加密存储

--- app/models/user/user_curl.go
+++ app/models/user/user_curl.go
@@ -1,13 +1,18 @@
 // Package user 存放用户 Model 相关逻辑
-// 本文件处理 user 的数据库相关操作
 // 方法的接收者为 (userModel *User)
 package user
 
 import (
 	"gohub/pkg/database"
+	"gohub/pkg/hash"
 )
 
 // Create 创建用户，通过 User.ID 来判断是否创建成功
 func (userModel *User) Create() {
 	database.DB.Create(&userModel)
+}
+
+// ComparePassword 密码是否正确
+func (userModel *User) ComparePassword(_password string) bool {
+	return hash.BcryptCheck(_password, userModel.Password)
 }
