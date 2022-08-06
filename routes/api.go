81d90e01842e429937bae2b6ed5f5f83f42dd48a
AsuraTu <455224159@qq.com>
2022-08-06 21:54:58 +0800

    限流中间件

--- routes/api.go
+++ routes/api.go
@@ -11,7 +11,7 @@ import (
 // RegisterAPIRoutes 注册 API 相关路由
 func RegisterAPIRoutes(r *gin.Engine) {
 
-	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
+	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里。
 	v1 := r.Group("/v1")
 
 	// 全局限流中间件：每小时限流。这里是所有 API （根据 IP）请求加起来。
