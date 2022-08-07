72520e457bbb58c7c184d9489d17f2298c84b17c
AsuraTu <455224159@qq.com>
2022-08-07 13:23:29 +0800

    用户列表

--- app/http/controllers/api/v1/users_controller.go
+++ app/http/controllers/api/v1/users_controller.go
@@ -1,18 +1,25 @@
 package v1
 
 import (
-    "gohub/pkg/auth"
-    "gohub/pkg/response"
+	"gohub/app/models/user"
+	"gohub/pkg/auth"
+	"gohub/pkg/response"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 type UsersController struct {
-    BaseAPIController
+	BaseAPIController
 }
 
 // CurrentUser 当前登录用户信息
 func (ctrl *UsersController) CurrentUser(c *gin.Context) {
-    userModel := auth.CurrentUser(c)
-    response.Data(c, userModel)
+	userModel := auth.CurrentUser(c)
+	response.Data(c, userModel)
+}
+
+// Index 所有用户
+func (ctrl *UsersController) Index(c *gin.Context) {
+	data := user.All()
+	response.Data(c, data)
 }
