31a7897fe4fc8e922ed8873569a0a42ae6dbd4c1
AsuraTu <455224159@qq.com>
2022-08-07 11:48:55 +0800

    seed 命令

--- database/seeders/seeder.go
+++ database/seeders/seeder.go
@@ -5,10 +5,10 @@ import "gohub/pkg/seed"
 
 func Initialize() {
 
-    // 触发加载本目录下其他文件中的 init 方法
+	// 触发加载本目录下其他文件中的 init 方法
 
-    // 指定优先于同目录下的其他文件运行
-    seed.SetRunOrder([]string{
-        "SeedUsersTable",
-    })
+	// 指定优先于同目录下的其他文件运行
+	seed.SetRunOrder([]string{
+		"SeedUsersTable",
+	})
 }
