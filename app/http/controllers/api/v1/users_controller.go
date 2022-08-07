5fff493bcf9fd5b51f8639054cfd5336d0b8bdf2
AsuraTu <455224159@qq.com>
2022-08-07 17:18:54 +0800

    修改密码

--- app/http/controllers/api/v1/users_controller.go
+++ app/http/controllers/api/v1/users_controller.go
@@ -1,90 +1,112 @@
 package v1
 
 import (
-    "gohub/app/models/user"
-    "gohub/app/requests"
-    "gohub/pkg/auth"
-    "gohub/pkg/response"
+	"gohub/app/models/user"
+	"gohub/app/requests"
+	"gohub/pkg/auth"
+	"gohub/pkg/response"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 type UsersController struct {
-    BaseAPIController
+	BaseAPIController
 }
 
 // CurrentUser 当前登录用户信息
 func (ctrl *UsersController) CurrentUser(c *gin.Context) {
-    userModel := auth.CurrentUser(c)
-    response.Data(c, userModel)
+	userModel := auth.CurrentUser(c)
+	response.Data(c, userModel)
 }
 
 // Index 所有用户
 func (ctrl *UsersController) Index(c *gin.Context) {
-    request := requests.PaginationRequest{}
-    if ok := requests.Validate(c, &request, requests.Pagination); !ok {
-        return
-    }
+	request := requests.PaginationRequest{}
+	if ok := requests.Validate(c, &request, requests.Pagination); !ok {
+		return
+	}
 
-    data, pager := user.Paginate(c, 10)
-    response.JSON(c, gin.H{
-        "data":  data,
-        "pager": pager,
-    })
+	data, pager := user.Paginate(c, 10)
+	response.JSON(c, gin.H{
+		"data":  data,
+		"pager": pager,
+	})
 }
 
 func (ctrl *UsersController) UpdateProfile(c *gin.Context) {
 
-    request := requests.UserUpdateProfileRequest{}
-    if ok := requests.Validate(c, &request, requests.UserUpdateProfile); !ok {
-        return
-    }
+	request := requests.UserUpdateProfileRequest{}
+	if ok := requests.Validate(c, &request, requests.UserUpdateProfile); !ok {
+		return
+	}
 
-    currentUser := auth.CurrentUser(c)
-    currentUser.Name = request.Name
-    currentUser.City = request.City
-    currentUser.Introduction = request.Introduction
-    rowsAffected := currentUser.Save()
-    if rowsAffected > 0 {
-        response.Data(c, currentUser)
-    } else {
-        response.Abort500(c, "更新失败，请稍后尝试~")
-    }
+	currentUser := auth.CurrentUser(c)
+	currentUser.Name = request.Name
+	currentUser.City = request.City
+	currentUser.Introduction = request.Introduction
+	rowsAffected := currentUser.Save()
+	if rowsAffected > 0 {
+		response.Data(c, currentUser)
+	} else {
+		response.Abort500(c, "更新失败，请稍后尝试~")
+	}
 }
 
 func (ctrl *UsersController) UpdateEmail(c *gin.Context) {
 
-    request := requests.UserUpdateEmailRequest{}
-    if ok := requests.Validate(c, &request, requests.UserUpdateEmail); !ok {
-        return
-    }
+	request := requests.UserUpdateEmailRequest{}
+	if ok := requests.Validate(c, &request, requests.UserUpdateEmail); !ok {
+		return
+	}
 
-    currentUser := auth.CurrentUser(c)
-    currentUser.Email = request.Email
-    rowsAffected := currentUser.Save()
+	currentUser := auth.CurrentUser(c)
+	currentUser.Email = request.Email
+	rowsAffected := currentUser.Save()
 
-    if rowsAffected > 0 {
-        response.Success(c)
-    } else {
-        // 失败，显示错误提示
-        response.Abort500(c, "更新失败，请稍后尝试~")
-    }
+	if rowsAffected > 0 {
+		response.Success(c)
+	} else {
+		// 失败，显示错误提示
+		response.Abort500(c, "更新失败，请稍后尝试~")
+	}
 }
 
 func (ctrl *UsersController) UpdatePhone(c *gin.Context) {
 
-    request := requests.UserUpdatePhoneRequest{}
-    if ok := requests.Validate(c, &request, requests.UserUpdatePhone); !ok {
-        return
-    }
+	request := requests.UserUpdatePhoneRequest{}
+	if ok := requests.Validate(c, &request, requests.UserUpdatePhone); !ok {
+		return
+	}
 
-    currentUser := auth.CurrentUser(c)
-    currentUser.Phone = request.Phone
-    rowsAffected := currentUser.Save()
+	currentUser := auth.CurrentUser(c)
+	currentUser.Phone = request.Phone
+	rowsAffected := currentUser.Save()
 
-    if rowsAffected > 0 {
-        response.Success(c)
-    } else {
-        response.Abort500(c, "更新失败，请稍后尝试~")
-    }
+	if rowsAffected > 0 {
+		response.Success(c)
+	} else {
+		response.Abort500(c, "更新失败，请稍后尝试~")
+	}
+}
+
+func (ctrl *UsersController) UpdatePassword(c *gin.Context) {
+
+	request := requests.UserUpdatePasswordRequest{}
+	if ok := requests.Validate(c, &request, requests.UserUpdatePassword); !ok {
+		return
+	}
+
+	currentUser := auth.CurrentUser(c)
+	// 验证原始密码是否正确
+	_, err := auth.Attempt(currentUser.Name, request.Password)
+	if err != nil {
+		// 失败，显示错误提示
+		response.Unauthorized(c, "原密码不正确")
+	} else {
+		// 更新密码为新密码
+		currentUser.Password = request.NewPassword
+		currentUser.Save()
+
+		response.Success(c)
+	}
 }
