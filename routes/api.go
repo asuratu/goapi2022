5bea032fca4e4a0fe2554e42504e7ae94880df26
AsuraTu <455224159@qq.com>
2022-08-07 17:12:02 +0800

    编辑个人资料

--- routes/api.go
+++ routes/api.go
@@ -59,6 +59,7 @@ func RegisterAPIRoutes(r *gin.Engine) {
 		usersGroup := v1.Group("/users")
 		{
 			usersGroup.GET("", uc.Index)
+			usersGroup.PUT("", middlewares.AuthJWT(), uc.UpdateProfile)
 		}
 
 		cgc := new(controllers.CategoriesController)
@@ -86,5 +87,6 @@ func RegisterAPIRoutes(r *gin.Engine) {
 		{
 			linksGroup.GET("", lsc.Index)
 		}
+
 	}
 }
